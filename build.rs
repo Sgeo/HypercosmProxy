b849aac6e47ead4b15cecdffb86ac32195f3274e
Sgeo <sgeoster@gmail.com>
2021-01-17 00:37:37 -0500

    Rename rc file

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     let mut rc = WindowsResource::new();
-    rc.set_resource_file("nphypercosm.rc");
+    rc.set_resource_file("NPAxHyp.rc");
     rc.compile()?;
     Ok(())
 }
